e518e05e1fdb5de2731d345cc258612e50c43491
vishal <vishalsanap663@gmail.com>
2024-12-06 19:42:47 +0530

    update frontend deployed link in backend

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 const Place = require("./models/Place.js");
-const Booking = require("./models/Booking.js")
+const Booking = require("./models/Booking.js");
 const cookieParser = require("cookie-parser");
 const imageDownloader = require("image-downloader");
 const multer = require("multer");
@@ -23,16 +23,16 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173",
+    origin: "https://airbnb-clone-pnwc.vercel.app/",
   })
 );
 
 function getUserDataFromReq(req) {
-  return new Promise((resolve,reject) => {
-  jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
-    if(err) throw err;
-    resolve(userData);
-  });
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
   });
 }
 
@@ -130,82 +130,125 @@ app.post("/upload-by-link", async (req, res) => {
     res.json(newName);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "An error occurred while downloading the image." });
+    res
+      .status(500)
+      .json({ error: "An error occurred while downloading the image." });
   }
 });
 
-app.post('/places',(req,res) =>{
+app.post("/places", (req, res) => {
   const { token } = req.cookies;
   const {
-      title,address,addedPhotos,description,
-      perks,extraInfo,checkIn,checkOut,maxGuests,price
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
   } = req.body;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-      if (err) throw err;
-      const placeDoc = await Place.create({
-          owner:userData.id,title,address,photos:addedPhotos,description,
-          perks,extraInfo,checkIn,checkOut,maxGuests,price,
-      });
-      res.json(placeDoc); 
-  }); 
-});
-
-
-app.get('/user-places', (req,res) => {
-  const { token } = req.cookies;
-  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
-    const {id} = userData;
-    res.json( await Place.find({owner:id}))
+    if (err) throw err;
+    const placeDoc = await Place.create({
+      owner: userData.id,
+      title,
+      address,
+      photos: addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+    });
+    res.json(placeDoc);
   });
 });
 
+app.get("/user-places", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    const { id } = userData;
+    res.json(await Place.find({ owner: id }));
+  });
+});
 
-app.get('/places/:id', async (req, res) => {
+app.get("/places/:id", async (req, res) => {
   const { id } = req.params;
   res.json(await Place.findById(id));
 });
 
-app.put('/places', async (req,res) => {
+app.put("/places", async (req, res) => {
   const { token } = req.cookies;
   const {
-      id,title,address,addedPhotos,description,
-      perks,extraInfo,checkIn,checkOut,maxGuests,price,
+    id,
+    title,
+    address,
+    addedPhotos,
+    description,
+    perks,
+    extraInfo,
+    checkIn,
+    checkOut,
+    maxGuests,
+    price,
   } = req.body;
-  
+
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     const placeDoc = await Place.findById(id);
-    if(userData.id === placeDoc.owner.toString()) {
+    if (userData.id === placeDoc.owner.toString()) {
       placeDoc.set({
-        title,address,photos:addedPhotos,description,
-        perks,extraInfo,checkIn,checkOut,maxGuests,price,
-      })
+        title,
+        address,
+        photos: addedPhotos,
+        description,
+        perks,
+        extraInfo,
+        checkIn,
+        checkOut,
+        maxGuests,
+        price,
+      });
       await placeDoc.save();
-      res.json('ok');
+      res.json("ok");
     }
   });
 });
 
-app.get('/places', async (req,res) => {
-  res.json(await Place.find() );
+app.get("/places", async (req, res) => {
+  res.json(await Place.find());
 });
 
-app.post('/bookings', async (req,res) => {
+app.post("/bookings", async (req, res) => {
   const userData = await getUserDataFromReq(req);
-  const{place,checkIn,checkOut,numberOfGuests,name,phone,price} = req.body;
+  const { place, checkIn, checkOut, numberOfGuests, name, phone, price } =
+    req.body;
   Booking.create({
-    place,checkIn,checkOut,numberOfGuests,name,phone,price,
-    user:userData.id,
-  }).then((doc) => {
-    res.json(doc);
-  }).catch((err) => {
-    throw err;
-  });
+    place,
+    checkIn,
+    checkOut,
+    numberOfGuests,
+    name,
+    phone,
+    price,
+    user: userData.id,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      throw err;
+    });
 });
 
-app.get('/bookings', async (req,res) => {
+app.get("/bookings", async (req, res) => {
   const userData = await getUserDataFromReq(req);
-  res.json(await Booking.find({user:userData.id}).populate('place'));
-})
+  res.json(await Booking.find({ user: userData.id }).populate("place"));
+});
 
 app.listen(4000, () => {
   console.log("Server is listening on port 4000");
